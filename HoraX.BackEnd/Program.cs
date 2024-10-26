21fe7fbd0ae17718ea2a4749235a7fbc5132d7f0
4lvinh0 <edunascimentoalvess@gmail.com>
2024-10-26 20:10:06 -0300

    Alteração Program.cs para o BD

--- HoraX.BackEnd/Program.cs
+++ HoraX.BackEnd/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using HoraX.BackEnd.Data;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options 
+    => options.UseInMemoryDatabase("Banco para testes"));
+
+
 var app = builder.Build();
+
+
+
+
 
 app.MapGet("/", () => "Hello World!");
 
